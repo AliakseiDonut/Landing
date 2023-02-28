f219e64ddf29b37ac838a0cd061ee574108ecb9d
aliakseidonut <alexeydomut@gmail.com>
2023-02-28 23:55:42 +0300

    d

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const burgerMenu = document.querySelector('.burger-menu');
-const line = document.querySelectorAll(".line");
 const menu = document.querySelector(".menu");
 
-
 burgerMenu.addEventListener('click',(event) => {
-    line[0].classList.toggle('first-line-active');
-    line[1].classList.toggle('hidden');
-    line[2].classList.toggle('second-line-active');
     menu.classList.toggle("hidden");
 })
