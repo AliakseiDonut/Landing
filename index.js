616ab8f7ea5c0d0b8cdbfc972fef0666dff72967
aliakseidonut <alexeydomut@gmail.com>
2023-03-02 10:05:20 +0300

    fix burger-menu

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const menu = document.querySelector(".menu");
+const lines = document.querySelectorAll(".line");
 
 burgerMenu.addEventListener('click',(event) => {
-    menu.classList.toggle("hidden");
+    menu.classList.toggle("menu-hidden");
+    burgerMenu.classList.toggle("burger-menu-active");
+    lines.forEach(element => {
+        element.classList.toggle("line-active");
+    });
 })
