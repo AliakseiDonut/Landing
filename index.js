2ee1ed5de4b5599936beddd891d116dad3e41695
aliakseidonut <alexeydomut@gmail.com>
2023-03-06 20:49:38 +0300

    add fixed header

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const menu = document.querySelector(".menu");
 const lines = document.querySelectorAll(".line");
+const header = document.getElementById("header");
 
 burgerMenu.addEventListener('click',(event) => {
     menu.classList.toggle("menu-hidden");
@@ -8,4 +9,14 @@ burgerMenu.addEventListener('click',(event) => {
     lines.forEach(element => {
         element.classList.toggle("line-active");
     });
+})
+
+window.addEventListener('scroll', event => {
+    if(window.innerWidth > 768){
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            header.classList.add('header__after-scroll');
+          } else {
+            header.classList.remove('header__after-scroll');
+          }
+    }
 })
